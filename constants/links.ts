da276116e6226a58fce21fc0e4668b6de233903e
Tecston <a01254620@tec.mx>
2026-09-17 13:04:08 -0700

    fix: restore landing build utilities

---
+++ constants/links.ts
@@ -0,0 +1,9 @@
+export const CONSTANTS = {
+  LOGIN_LINK: "/login",
+  REGISTER_LINK: "/register",
+  CALCOM_NAMESPACE: "chat-with-manu-demo",
+  CALCOM_BRAND_COLOR: "#000000",
+  CALCOM_HIDE_EVENT_TYPE_DETAILS: false,
+  CALCOM_LAYOUT: "month_view",
+  CALCOM_LINK: "manu-arora-vesr9s/chat-with-manu-demo",
+};
